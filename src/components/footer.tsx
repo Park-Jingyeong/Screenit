b92f69ecb23e8d5a62ce16eb2b0fc9e66bbd31b1
Park-Jingyeong <pjg0305pjg@gmail.com>
2025-05-22 22:07:31 +0900

    Feat: header 반응형 웹 수정

---
+++ src/components/footer.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <Link href={"/"}>홈</Link>
+          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </footer>
+  );
+}
